8d559ca94161d40629be836dd3cbf93477b6b46e
Francisco Javier Jimenez <>
2024-10-08 09:16:39 +0200

    Add: instead of

--- POO.js
+++ POO.js
@@ -20,8 +20,25 @@ class Gerente extends Empleado{
     }
 }
 
+function imprimir(tipo){
+    console.log(tipo.obtenerDetalles());
+    if(tipo instanceof Empleado){
+        console.log("objeto tipo empleado");
+        console.log(tipo.departamento)
+    }
+    else if(tipo instanceof Gerente){
+        console.log("objeto tipo gerente");
+    }
+    else if(tipo instanceof Object){
+        console.log("objeto tipo Object");
+    }
+}
+
 let empleado1 = new Empleado('Juan', 3000);
 console.log( empleado1.obtenerDetalles() );
 
 let gerente1 = new Gerente('Carlos', 5000, 'Sistemas');
 console.log( gerente1.obtenerDetalles() );
+
+imprimir(empleado1);
+imprimir(gerente1);

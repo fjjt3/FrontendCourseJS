843b3ab8ca6e88e1ca5ae6f4e5a18df68441ab81
Francisco Javier Jimenez <>
2024-10-14 09:25:10 +0200

    Add: Arrays ingresos, egresos

---
+++ Presupuesto/js/app.js
@@ -0,0 +1,17 @@
+const ingresos = [
+    new Ingreso('Salario', 2100.00),
+    new Ingreso('Venta', 1500)
+];
+
+const egresos = [
+    new Egreso('Alquiler', 900),
+    new Egreso('ropa', 300)
+];
+
+let cargarApp = () =>{
+    cargarCabacero();
+}
+
+let cargarCabacero = () => {
+
+}
